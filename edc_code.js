f467cb4e6a244d14ebb20e18917c6cf2c5529a0d
harrietta.potter <harrietta.potter@ac49d6d4-4d2f-0410-99e5-c9e5b8216aa7>
2007-05-03 00:26:26 +0000

    Biatch

--- edc_code.js
+++ edc_code.js
@@ -8,8 +8,8 @@
  	rect = document.getElementById("field");
  	width = parseInt(rect.getAttributeNS(null, "width"));
  	height = parseInt(rect.getAttributeNS(null, "height"));
- 	width += 1;
- 	height -= 1;
+ 	width += 2;
+ 	height -= 2;
  	rect.setAttributeNS(null, "width", width.toString(10))
  	rect.setAttributeNS(null, "height", height.toString(10))
  }
@@ -22,8 +22,8 @@
  	rect = document.getElementById("field");
  	width = parseInt(rect.getAttributeNS(null, "width"));
  	height = parseInt(rect.getAttributeNS(null, "height"));
- 	width -= 1;
- 	height += 1;
+ 	width -= 2;
+ 	height += 2;
  	rect.setAttributeNS(null, "width", width.toString(10))
  	rect.setAttributeNS(null, "height", height.toString(10))
   	  	
